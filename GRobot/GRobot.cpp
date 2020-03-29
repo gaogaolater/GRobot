cae9b464d4b16146148400cf77291cc131212c44
gaogao <362115359@qq.com>
2020-03-29 23:52:34 +0800

    update ignore

--- GRobot/GRobot.cpp
+++ GRobot/GRobot.cpp
@@ -4,7 +4,12 @@
 #include "framework.h"
 #include "resource.h"
 #include "GRobot.h"
+#include <tchar.h>
+#include <string.h>
+#include <string>
+using namespace std;
 
+void startWx();
 INT_PTR CALLBACK wechatRobot(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
@@ -40,7 +45,7 @@ INT_PTR CALLBACK wechatRobot(
 		switch (wParam)
 		{
 		case ID_STARTWX:
-			MessageBoxA(hDlg, "开始按钮", "提示", NULL);
+			startWx();
 			break;
 		case ID_CLOSEWX:
 			MessageBoxA(hDlg, "关闭按钮", "提示", NULL);
@@ -55,4 +60,19 @@ INT_PTR CALLBACK wechatRobot(
 		break;
 	}
 	return (INT_PTR)FALSE;
+}
+
+void startWx() {
+	//注意路径一定不能写错
+	wchar_t path[] = L"C:\\Program Files (x86)\\Tencent\\WeChat\\WeChat.exe";
+	STARTUPINFO si;
+	memset(&si, 0, sizeof(si));
+	si.cb = sizeof(si);
+	PROCESS_INFORMATION pi;
+	memset(&pi, 0, sizeof(pi));
+	BOOL bStatus = CreateProcess(path, NULL, NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
+	if (bStatus == FALSE) {
+		MessageBox(NULL, L"启动失败", L"提示", 0);
+		return;
+	}
 }
